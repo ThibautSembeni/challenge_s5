7e679449d6d8ce1ff52b6eba4edba0ececbf8ff1
mchemlal <michel.chemlal@gmail.com>
2024-02-13 11:24:34 +0100

    translation organism appointment preview

--- client/src/components/organisms/Appointment/preview.jsx
+++ client/src/components/organisms/Appointment/preview.jsx
@@ -6,8 +6,11 @@ import {
 import { CalendarIcon, ClockIcon } from "@heroicons/react/24/outline";
 import { Icon } from "@/components/atoms/Icons/Icon.jsx";
 import { displayDay, displayTime } from "@/utils/date.js";
+import { useTranslation } from "react-i18next";
+
 
 export default function AppointmentPreview({ appointment, onClick, selected }) {
+    const { t } = useTranslation();
     return (
         <div
             className={`lg:col-start-3 lg:row-end-1 ${
@@ -15,7 +18,7 @@ export default function AppointmentPreview({ appointment, onClick, selected }) {
             } hover:scale-105`}
             onClick={() => onClick()}
         >
-            <h2 className="sr-only">Rendez-vous</h2>
+            <h2 className="sr-only">{t("components.organisms.appointment.preview.h2RendezVous")}</h2>
             <div className="rounded-lg bg-gray-50 shadow-sm ring-1 ring-gray-900/5">
                 <dl className="flex flex-wrap pb-6">
                     <div className="flex-auto pl-6 pt-6">
@@ -33,20 +36,20 @@ export default function AppointmentPreview({ appointment, onClick, selected }) {
                                 </span>
                             </div>
                             <div className="flex-none self-end px-6 pt-4">
-                                <dt className="sr-only">Status</dt>
+                                <dt className="sr-only">{t("components.organisms.appointment.preview.divDtSatus")}</dt>
                                 {appointment.status === "scheduled" && (
                                     <dd className="inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">
-                                        A venir
+                                        {t("components.organisms.appointment.preview.divDtDD")}
                                     </dd>
                                 )}
                                 {appointment.status === "completed" && (
                                     <dd className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
-                                        Passé
+                                        {t("components.organisms.appointment.preview.divDtDD2")}
                                     </dd>
                                 )}{" "}
                                 {appointment.status === "cancelled" && (
                                     <dd className="inline-flex items-center rounded-md bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/20">
-                                        Annulé
+                                        {t("components.organisms.appointment.preview.divDtDD3")}
                                     </dd>
                                 )}
                             </div>
@@ -54,7 +57,7 @@ export default function AppointmentPreview({ appointment, onClick, selected }) {
                     </div>
                     <div className="mt-6 flex w-full flex-none gap-x-4 border-t border-gray-900/5 px-6 pt-6">
                         <dt className="flex-none">
-                            <span className="sr-only">Vétérinaire</span>
+                            <span className="sr-only">{t("components.organisms.appointment.preview.divDtSpanVeterinaire")}</span>
                             <UserCircleIcon
                                 className="h-6 w-5 text-gray-400"
                                 aria-hidden="true"
@@ -68,7 +71,7 @@ export default function AppointmentPreview({ appointment, onClick, selected }) {
                     </div>
                     <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
                         <dt className="flex-none">
-                            <span className="sr-only">Due date</span>
+                            <span className="sr-only">{t("components.organisms.appointment.preview.divDtSpanDueDate")}</span>
                             <Icon
                                 icon={"pets"}
                                 className="h-6 w-5 text-gray-400 bg-gray-400"
@@ -83,14 +86,14 @@ export default function AppointmentPreview({ appointment, onClick, selected }) {
                     </div>
                     <div className="mt-4 flex w-full flex-none gap-x-4 px-6">
                         <dt className="flex-none">
-                            <span className="sr-only">Status</span>
+                            <span className="sr-only">{t("components.organisms.appointment.preview.divDtSpanSatus")}</span>
                             <CreditCardIcon
                                 className="h-6 w-5 text-gray-400"
                                 aria-hidden="true"
                             />
                         </dt>
                         <dd className="text-sm leading-6 text-gray-500">
-                            Paid with MasterCard
+                            {t("components.organisms.appointment.preview.divDDPaidWithMastercard")}
                         </dd>
                     </div>
                 </dl>
