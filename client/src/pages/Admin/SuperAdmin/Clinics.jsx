3c7377e74263a799ff674a4a5fd22861375425d6
Romain LETHUMIER <romain.lethumier@gmail.com>
2024-02-15 13:45:31 +0100

    fix logout admin

--- client/src/pages/Admin/SuperAdmin/Clinics.jsx
+++ client/src/pages/Admin/SuperAdmin/Clinics.jsx
@@ -237,7 +237,6 @@ export default function Clinics() {
 
             <div className="lg:pl-72">
               <TopSideBar
-                navigation={userNavigation}
                 sidebarOpen={sidebarOpen}
                 setSidebarOpen={setSidebarOpen}
               />
