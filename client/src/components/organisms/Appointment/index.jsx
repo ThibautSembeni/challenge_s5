8abb6422b4ae3e9f4a41aae7169904f129d53795
Choetsu <samy.amh94@gmail.com>
2024-02-15 21:36:24 +0100

    Ajouts de l'export au format csv et pdf des rendez-vous à venir pour un vétérinaire

--- client/src/components/organisms/Appointment/index.jsx
+++ client/src/components/organisms/Appointment/index.jsx
@@ -1,47 +1,85 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Table from "@/components/atoms/Table/Table";
 import { DocumentIcon } from "@heroicons/react/24/outline";
-import { getAllAppointments } from "@/api/appointments/index";
-
-const people = [
-    {
-        name: "Lindsay Walton",
-        title: "Front-end Developer",
-        email: "lindsay.walton@example.com",
-        role: "Member",
-    },
-    {
-        name: "Emily Foster",
-        title: "Designer",
-        email: "enfoenuof@fjfej.eif",
-        role: "Admin",
-    },
-    {
-        name: "Jenny Wilson",
-        title: "Human Resources",
-        email: "vefeé",
-        role: "Admin",
-    },
-];
+import { getAllAppointmentsOfVeterinarian } from "@/api/appointments/index";
+import { useAuth } from "@/contexts/AuthContext";
+import { jsPDF } from "jspdf";
 
 export default function Appointments() {
     const [appointmentsScheduled, setAppointmentsScheduled] = useState([]);
-    const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(false);
+    const { user } = useAuth();
+    const vetUuid = user.veterinarian.uuid;
 
-    const handleGetAppointmentsScheduled = async () => {
-        const result = await getAllAppointments();
-        const appointmentsScheduled = result.data["hydra:member"].filter(
+    const handleGetAppointmentsScheduled = useCallback(async () => {
+        setIsLoading(true);
+        const result = await getAllAppointmentsOfVeterinarian(vetUuid);
+        const appointments = result.data["hydra:member"].filter(
             (appointment) => appointment.status === "scheduled"
         );
-        setAppointmentsScheduled(appointmentsScheduled);
+
+        const newAppointmentsScheduled = appointments.map((appointment) => ({
+            lastname: appointment.userID.lastname,
+            firstname: appointment.userID.firstname,
+            phone: appointment.userID.phone,
+            date: convertDate(appointment.date),
+        }));
+
+        setAppointmentsScheduled(newAppointmentsScheduled);
         setIsLoading(false);
-    };
+    }, [vetUuid]);
 
     useEffect(() => {
         handleGetAppointmentsScheduled();
-    }, []);
+    }, [handleGetAppointmentsScheduled]);
 
-    console.log(appointmentsScheduled);
+    const convertDate = (date) => {
+        const dateObject = new Date(date);
+        return dateObject.toLocaleDateString("fr-FR");
+    };
+
+    const exportAppointmentsToCSV = () => {
+        const headers = ["Nom", "Prénom", "Téléphone", "Date"].join(",");
+        const csv = appointmentsScheduled.map((appointment) => {
+            return [
+                appointment.lastname,
+                appointment.firstname,
+                appointment.phone,
+                appointment.date,
+            ].join(",");
+        });
+
+        const csvContent = [headers, ...csv].join("\n");
+        const csvData = new Blob([csvContent], { type: "text/csv" });
+
+        const csvUrl = URL.createObjectURL(csvData);
+
+        const a = document.createElement("a");
+        a.href = csvUrl;
+        a.download = "rendez-vous.csv";
+        a.click();
+    };
+
+    const exportAppointmentsToPDF = () => {
+        const doc = new jsPDF();
+        const headers = ["Nom", "Prénom", "Téléphone", "Date"];
+
+        let y = 10;
+        headers.forEach((header, index) => {
+            doc.text(header, 10 + index * 40, y);
+        });
+        y += 10;
+
+        appointmentsScheduled.forEach((appointment) => {
+            doc.text(appointment.lastname, 10, y);
+            doc.text(appointment.firstname, 50, y);
+            doc.text(appointment.phone, 90, y);
+            doc.text(appointment.date, 130, y);
+            y += 10;
+        });
+
+        doc.save("rendez-vous.pdf");
+    };
 
     return (
         <div className="px-4 sm:px-6 lg:px-8">
@@ -54,38 +92,54 @@ export default function Appointments() {
                         Liste des rendez-vous à venir
                     </p>
                 </div>
-                <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                <div className="mt-4 sm:ml-6 sm:mt-0 sm:flex-none">
                     <button
                         type="button"
                         className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+                        onClick={exportAppointmentsToCSV}
                     >
                         <DocumentIcon className="-ml-1 mr-2 h-5 w-5" />
-                        Exporter
+                        Export CSV
+                    </button>
+                </div>
+                <div className="mt-4 sm:ml-6 sm:mt-0 sm:flex-none">
+                    <button
+                        type="button"
+                        className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-indigo-500"
+                        onClick={exportAppointmentsToPDF}
+                    >
+                        <DocumentIcon className="-ml-1 mr-2 h-5 w-5" />
+                        Export PDF
                     </button>
                 </div>
             </div>
             <div className="mt-8 flow-root">
                 <Table
-                    data={people}
+                    data={appointmentsScheduled}
                     columns={[
                         {
-                            Header: "Name",
-                            accessor: "name",
+                            Header: "Nom",
+                            accessor: "lastname",
                         },
                         {
-                            Header: "Title",
-                            accessor: "title",
+                            Header: "Prénom",
+                            accessor: "firstname",
                         },
                         {
-                            Header: "Email",
-                            accessor: "email",
+                            Header: "Téléphone",
+                            accessor: "phone",
                         },
                         {
-                            Header: "Role",
-                            accessor: "role",
+                            Header: "Date",
+                            accessor: "date",
                         },
                     ]}
                 />
+                {isLoading && (
+                    <div className="flex justify-center items-center">
+                        <div className="animate-spin rounded-full h-8 w-8 border-b-2 border-blue-500"></div>
+                    </div>
+                )}
             </div>
         </div>
     );
