5d2db9b55e5deece27eef0fda094c83bc9315d15
ThibautSembeni <thibaut.sbn.dev@gmail.com>
2024-02-16 05:35:36 +0100

    fix(api): fix indentation and formatting in Payments.jsx to improve code readability fix(api): update route path for createPayments in routes.yaml to match the actual endpoint path feat(api): add authentication requirement for create-payments endpoint in security.yaml to restrict access

--- client/src/api/payments/Payments.jsx
+++ client/src/api/payments/Payments.jsx
@@ -2,11 +2,11 @@ import axiosInstance from "@/utils/axiosInstance.js";
 import axios from "axios";
 
 export const getAllPayments = async ({
-    page = 1,
-    itemsPerPage = 30,
-    pagination = false,
-    ...filters
-  } = {}) => {
+  page = 1,
+  itemsPerPage = 30,
+  pagination = false,
+  ...filters
+} = {}) => {
   const params = new URLSearchParams({
     page,
     itemsPerPage,
@@ -20,27 +20,33 @@ export const getAllPayments = async ({
 
 export const updateOnePayments = async (id, { amount, status }) => {
   try {
-    const response = await axiosInstance.patch(`/payments/${id}`, {
-      amount, status
-    }, {
-      headers: {
-        'Content-Type': 'application/merge-patch+json'
-      }
-    });
+    const response = await axiosInstance.patch(
+      `/payments/${id}`,
+      {
+        amount,
+        status,
+      },
+      {
+        headers: {
+          "Content-Type": "application/merge-patch+json",
+        },
+      },
+    );
 
     return { success: true };
   } catch (error) {
     console.error("Erreur lors de la mise à jour du paiement : ", error);
-    return { success: false, message: "Une erreur est survenue lors de la mise à jour du paiement" };
+    return {
+      success: false,
+      message: "Une erreur est survenue lors de la mise à jour du paiement",
+    };
   }
 };
 
-export const createPayments = async ({
-    paymentMethod
-  }) => {
+export const createPayments = async ({ paymentMethod }) => {
   try {
-    const payment = axiosInstance.post(`/createPayments`, {
-      paymentMethod
+    const payment = axiosInstance.post(`/create-payments`, {
+      paymentMethod,
     });
 
     return { success: true, payment };
